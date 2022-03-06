1e33c0dce802cc45921dd14a276e45a726108317
nawrasbelhadj <nawrasbelha@gmail.com>
2022-03-06 19:25:13 +0100

    entites and template

---
+++ src/Controller/courrierController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class courrierController extends AbstractController
+{
+    /**
+     * @Route("/history", name="history_courrier")
+     */
+    public function index(): Response
+    {
+        return $this->render('courrier/history.html.twig');
+    }
+
+    /**
+     * @Route("/alert", name="alert_courrier")
+     */
+    public function list(): Response
+    {
+        return $this->render('courrier/alert.html.twig');
+    }
+
+
+    /**
+     * @Route("/timeline", name="timeline_courrier")
+     */
+    public function show(): Response
+    {
+        return $this->render('courrier/timeline.html.twig');
+    }
+
+    /**
+     * @Route("/archive", name="archive_courrier")
+     */
+    public function search(): Response
+    {
+        return $this->render('courrier/archive.html.twig');
+    }
+}
---
+++ src/Entity/Post.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PostRepository::class)]
+class Post
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private $id;
+
+    #[ORM\Column(type: 'integer')]
+    private $postID;
+
+    #[ORM\Column(type: 'bigint')]
+    private $codePostal;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $city;
+
+    #[ORM\Column(type: 'text')]
+    private $street;
+
+    #[ORM\Column(type: 'string', length: 50)]
+    private $regionPost;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getPostID(): ?int
+    {
+        return $this->postID;
+    }
+
+    public function setPostID(int $postID): self
+    {
+        $this->postID = $postID;
+
+        return $this;
+    }
+
+    public function getCodePostal(): ?string
+    {
+        return $this->codePostal;
+    }
+
+    public function setCodePostal(string $codePostal): self
+    {
+        $this->codePostal = $codePostal;
+
+        return $this;
+    }
+
+    public function getCity(): ?string
+    {
+        return $this->city;
+    }
+
+    public function setCity(string $city): self
+    {
+        $this->city = $city;
+
+        return $this;
+    }
+
+    public function getStreet(): ?string
+    {
+        return $this->street;
+    }
+
+    public function setStreet(string $street): self
+    {
+        $this->street = $street;
+
+        return $this;
+    }
+
+    public function getRegionPost(): ?string
+    {
+        return $this->regionPost;
+    }
+
+    public function setRegionPost(string $regionPost): self
+    {
+        $this->regionPost = $regionPost;
+
+        return $this;
+    }
+}
