7995f39d91b5eafdecafb41137c0b9821f2d951f
rachedbelhadj <belhadj.rached@gmail.com>
2022-04-04 14:55:21 +0200

    fix code review

--- src/Form/Courrier/AddCourrierFormType.php
+++ src/Form/Courrier/AddCourrierFormType.php
@@ -1,34 +1,30 @@
 <?php
 
-namespace App\Form\post;
+namespace App\Form\Courrier;
 
-use App\Entity\Post;
+use App\Entity\Courrier;
 use Symfony\Component\Form\AbstractType;
-use Symfony\Component\Form\Extension\Core\Type\ResetType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-class AddPostFormType extends AbstractType
+
+class AddCourrierFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('codePostal', TextType::class )
-            ->add('city', TextType::class)
-            ->add('street', TextType::class)
-            ->add('regionPost', TextType::class)
-            ->add('save', SubmitType::class, ['label' => 'Save'])
-            ->add('reset', ResetType::class, ['label' => 'Reset']);
+            ->add('codeBarre', TextType::class)
+            ->add('postDeparture', TextType::class)
+            ->add('save', SubmitType::class, ['label' => 'Save']);
+            //->add('test', TextType::class);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Post::class,
+            'data_class' => Courrier::class,
         ]);
     }
-
-
 }
--- src/Form/Post/AddPostFormType.php
+++ src/Form/Post/AddPostFormType.php
@@ -1,30 +1,34 @@
 <?php
 
-namespace App\Form\courrier;
+namespace App\Form\Post;
 
-use App\Entity\Courrier;
+use App\Entity\Post;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ResetType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
-
-class AddCourrierFormType extends AbstractType
+class AddPostFormType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('codeBarre', TextType::class)
-            ->add('postDeparture', TextType::class)
-            ->add('save', SubmitType::class, ['label' => 'Save']);
-            //->add('test', TextType::class);
+            ->add('codePostal', TextType::class )
+            ->add('city', TextType::class)
+            ->add('street', TextType::class)
+            ->add('regionPost', TextType::class)
+            ->add('save', SubmitType::class, ['label' => 'Save'])
+            ->add('reset', ResetType::class, ['label' => 'Reset']);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
     {
         $resolver->setDefaults([
-            'data_class' => Courrier::class,
+            'data_class' => Post::class,
         ]);
     }
+
+
 }
